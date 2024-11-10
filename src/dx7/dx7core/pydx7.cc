421aa06006122045ee27d6b6304729b7e463f4af
bitfalder <felix.adler@gmx.de>
2024-11-10 22:55:24 +0100

    Refactor and use patch database from site_package location

--- src/dx7/dx7core/pydx7.cc
+++ src/dx7/dx7core/pydx7.cc
@@ -1,6 +1,6 @@
 
 #include <Python.h>
-//#include <iostream>
+
 #include <cstdlib>
 #include <math.h>
 #include "synth.h"
@@ -20,7 +20,11 @@
 #include "controllers.h"
 #include "dx7note.h"
 
-char * unpacked_patches = 0;
+#include <iostream>
+#include <filesystem>
+
+static char *unpacked_patches = 0;
+static PyObject *dx7_error = 0;
 
 void write_data(const int32_t *buf_in, short * buf_out, unsigned int * pos, int n) {
   int32_t delta = 0x100;
@@ -33,7 +37,6 @@ void write_data(const int32_t *buf_in, short * buf_out, unsigned int * pos, int 
   }
   *pos = *pos + n;
 }
-
 
 // Render but with 156 bytes of patch data
 short * render_patchdata(const char* patch_data, unsigned char midinote, unsigned char velocity, unsigned int samples, unsigned int keyup) {
@@ -64,15 +67,6 @@ short * render_patchdata(const char* patch_data, unsigned char midinote, unsigne
   return out;
 }
 
-// take in a patch #, a note # and a velocity and a sample length and a sample # to lift off a key?
-short * render(unsigned short patch, unsigned char midinote, unsigned char velocity, unsigned int samples, unsigned int keyup) {  
-  if( unpacked_patches == 0 )
-    return 0;
-  
-  const char* patch_data = &unpacked_patches[patch * 156];
-  return render_patchdata (patch_data, midinote, velocity, samples, keyup);
-}
-
 void init_synth(void) {
   double sample_rate = 44100.0;
   Freqlut::init(sample_rate);
@@ -83,20 +77,19 @@ void init_synth(void) {
 }
 
 static PyObject * render_patchdata_wrapper(PyObject *self, PyObject *args) {
-  int arg2, arg3, arg4, arg5;
   /* Default values. */
-  arg2 = 50; // midi note
-  arg3 = 70; // velocity
-  arg4 = 44100; // samples
-  arg5 = 22050; // keyup sample
+  int arg2 = 50; // midi note
+  int arg3 = 70; // velocity
+  int arg4 = 44100; // samples
+  int arg5 = 22050; // keyup sample
+  
   const char * patch_save;
   Py_ssize_t count;
   if (! PyArg_ParseTuple(args, "s#iiii", &patch_save, &count, &arg2, &arg3, &arg4, &arg5)) {
     return NULL;
   }
 
-  short * result;
-  result = render_patchdata(patch_save, arg2, arg3, arg4, arg5);
+  short * result = render_patchdata(patch_save, arg2, arg3, arg4, arg5);
   if( result == 0 )
     return 0;
 
@@ -113,21 +106,19 @@ static PyObject * render_patchdata_wrapper(PyObject *self, PyObject *args) {
 
 
 static PyObject * render_wrapper(PyObject *self, PyObject *args) {
-  int arg1, arg2, arg3, arg4, arg5;
-
   /* Default values. */
-  arg1 = 8; // patch #
-  arg2 = 50; // midi note
-  arg3 = 70; // velocity
-  arg4 = 44100; // samples
-  arg5 = 22050; // keyup sample
+  int arg1 = 8; // patch #
+  int arg2 = 50; // midi note
+  int arg3 = 70; // velocity
+  int arg4 = 44100; // samples
+  int arg5 = 22050; // keyup sample
 
   if (! PyArg_ParseTuple(args, "iiiii", &arg1, &arg2, &arg3, &arg4, &arg5)) {
     return NULL;
   }
 
-  short * result;
-  result = render(arg1, arg2, arg3, arg4, arg5);
+  const char* patch_data = &unpacked_patches[arg1 * 156];
+  short* result = render_patchdata(patch_data, arg2, arg3, arg4, arg5);
   if( result == 0 )
     return 0;
 
@@ -156,31 +147,55 @@ static PyObject * unpack_wrapper(PyObject *self, PyObject *args) {
   return ret;
 }
 
-static PyMethodDef DX7Methods[] = {
- {"render", render_wrapper, METH_VARARGS, "Render audio"},
- {"render_patchdata", render_patchdata_wrapper, METH_VARARGS, "Render audio via patch data"},
- {"unpack", unpack_wrapper, METH_VARARGS, "Unpack patch"},
- { NULL, NULL, 0, NULL }
-};
+//----------------------------------------------------------------------------------------
 
-static struct PyModuleDef dx7Def =
-{
-    PyModuleDef_HEAD_INIT,
-    "dx7", /* name of module */
-    "",          /* module documentation, may be NULL */
-    -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-    DX7Methods
-};
+/// @brief Build the filename of the database file by using __file__.
+/// @param module Pointer to existing python module instance
+/// @param filename Const reference to the filename without any path info
+/// @return String containing the fullpath to the database file
+std::string Dx7BuildDatabaseFilename(PyObject *module, const std::string& filename) {
+  PyObject *string = PyObject_GetAttrString(module, "__file__");
+  if( string == nullptr)
+    return filename;
 
-extern "C" {
-PyMODINIT_FUNC PyInit_dx7(void)
-{
-  // TODO: this file should go into the package directory from setup.py, but that is a PITA 
-  FILE *f = fopen("./bin/compact.bin","rb");
-  if( f == nullptr ) {
-    printf("dx7: compact.bin not found.\n" );
-    return nullptr;
+  std::string path( PyUnicode_AsUTF8(string) );
+  size_t found = path.find_last_of("/\\");
+  if( found == std::string::npos )
+    return filename;
+
+  std::string fullpath( path.substr(0,found) );
+  fullpath += "/dx7/bin/";
+  fullpath += filename;
+  
+  return fullpath;
+}
+
+/// @brief Execute DX7 module initialization as part of a multi-stage process. Here we have access to the __file__ variable! 
+/// @param module Pointer to the model instance
+/// @return 0 if successful, -1 otherwise. Set an exception if returning -1.
+int Dx7ExecuteModule(PyObject *module) {
+  
+  
+  dx7_error = PyErr_NewException("dx7.error", NULL, NULL);
+  if (PyModule_AddObjectRef(module, "error", dx7_error) < 0) {
+    Py_CLEAR(dx7_error);
+    Py_DECREF(module);
+    return -1;
   }
+
+  if (!PyObject_HasAttrString(module, "__file__")) {
+    PyErr_SetString(dx7_error, "Cannot derive database path from __file__!");
+    return -1;
+  }
+
+  std::string database_file = Dx7BuildDatabaseFilename(module, "compact.bin");
+
+  FILE *f = fopen(database_file.c_str(),"rb");
+  if( f == nullptr ) {
+    PyErr_SetString(dx7_error, "Patch database not found!");
+    return -1;
+  }
+
   // See how many voices are in the file
   fseek(f, 0, SEEK_END);
   long fsize = ftell(f);
@@ -196,11 +211,44 @@ PyMODINIT_FUNC PyInit_dx7(void)
   for(int i=0;i<patches;i++) {
     UnpackPatch(all_patches + (i*128), &unpacked_patches[i*156] );
   }
-
-  // Have to call this out as we're in C extern 
-  init_synth();
   free(all_patches);
-  return PyModule_Create(&dx7Def);
-}
+
+  init_synth();
+
+  return 0;
 }
 
+// ============================================================================
+// Python module structs and main entry code
+
+static PyMethodDef dx7_methods[] = {
+ {"render", render_wrapper, METH_VARARGS, "Render audio"},
+ {"render_patchdata", render_patchdata_wrapper, METH_VARARGS, "Render audio via patch data"},
+ {"unpack", unpack_wrapper, METH_VARARGS, "Unpack patch"},
+ { NULL, NULL, 0, NULL }
+};
+
+static struct PyModuleDef_Slot dx7_moduleslots[] = {
+    {Py_mod_exec, (void*) Dx7ExecuteModule},
+    {0, NULL}
+};
+
+static struct PyModuleDef dx7_module_definition =
+{
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "dx7",        /* name of module */
+    .m_doc = nullptr,            /* module documentation, may be NULL */
+    .m_size = 0,           /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+    .m_methods = dx7_methods,
+    .m_slots = dx7_moduleslots,
+};
+
+// This is the entry point for the multi-stage initialization!
+extern "C" {
+
+PyMODINIT_FUNC PyInit_dx7(void)
+{
+  return PyModuleDef_Init(&dx7_module_definition); 
+}
+
+}
