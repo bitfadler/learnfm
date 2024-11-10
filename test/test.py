83931d371276e1b751ad823f0667759bba8f79b3
bitfalder <felix.adler@gmx.de>
2024-11-10 23:00:09 +0100

    Add export to wav

--- test/test.py
+++ test/test.py
@@ -1,11 +1,21 @@
 import dx7
+import wave
+from array import array
 
-if __name__ == "__main__":
-    patch_number = 324 
-    midi_note = 60
+def generate_patch(patch, note) -> array:
+    print( f"Exporting patch {patch} (note: {note})")
     velocity = 99
     samples = 44100 * 10 
-    keyup_sample = 44100 * 5 # when to let the key up
-    data = dx7.render(patch_number, midi_note, velocity, samples, keyup_sample)
-    print( "Sample size: {}".format(len(data)))
+    keyup_sample = int(samples*0.5)
+    data = dx7.render(patch, note, velocity, samples, keyup_sample)
+    return array("i", data)
 
+
+if __name__ == "__main__":
+    a = generate_patch(324, 60)
+
+    with wave.open("output.wav", mode="wb") as wav_file:
+        wav_file.setnchannels(2)
+        wav_file.setsampwidth(2)
+        wav_file.setframerate(44100)
+        wav_file.writeframes(a)
